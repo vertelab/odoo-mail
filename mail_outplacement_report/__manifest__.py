ee2340b6cc9d95e11033f3536634015569412f0f
Aldin Draghoender <aldind@hotmail.com>
2021-01-18 17:17:59 +0100

    update on files

--- mail_outplacement_report/__manifest__.py
+++ mail_outplacement_report/__manifest__.py
@@ -2,11 +2,7 @@
 
 {
     "name": "DAFA Outplacement Templates",
-<<<<<<< HEAD
     "version": "12.0.2.0.0",
-=======
-    "version": "12.0.1.0.1",
->>>>>>> e23d5cedf130b6ff0389adb70c797372bbe5d016
     "author": "Vertel AB",
     "license": "AGPL-3",
     "website": "https://vertel.se/",
@@ -15,20 +11,17 @@
     "depends": [
         "hr",
         "outplacement",
-        "auth_signup",
+        #"auth_signup",
         "send_mail_nadim",
         ],
     "external_dependencies": [],
     "data": [
-        "views/employee_view.xml",
-<<<<<<< HEAD
-        "data/employee_login_data.xml",
-=======
+        "views/outplacement_view.xml",
         "reports/assigned_coach_report.xml",
+        "data/employee_login_data.xml",
         "data/assigned_coach_data.xml",
->>>>>>> e23d5cedf130b6ff0389adb70c797372bbe5d016
+        
     ],
     "application": False,
     "installable": True,
-    "auto_install": True,
 }
