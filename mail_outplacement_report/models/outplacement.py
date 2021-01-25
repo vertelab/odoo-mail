68f76f159f25d5375b6d66290959763a4df41720
Aldin Draghoender <aldind@hotmail.com>
2021-01-25 17:29:42 +0100

    12.0-AFC-1614  mail_outplacement_report - Välkomstbrevet har inte dynamiskt innehåll Added dynamic fields for ebrev and also fields for address, time and date.

--- mail_outplacement_report/models/outplacement.py
+++ mail_outplacement_report/models/outplacement.py
@@ -1,11 +1,14 @@
 from odoo import models, api, _, fields
+from datetime import datetime, timedelta, date
 import logging
 
 _logger = logging.getLogger(__name__)
 
+# LOCAL_TZ: Local timezone 
+LOCAL_TZ = 'Europe/Stockholm'
+
 class HrEmployee(models.Model):
     _inherit = "hr.employee"
-
     def action_send_email(self):
         self.ensure_one()
         ir_model_data = self.env['ir.model.data']
@@ -22,14 +25,33 @@ class HrEmployee(models.Model):
 
 class Outplacement(models.Model):
     _inherit = "outplacement"
+    venue = fields.Text(string="Performing adress", help="Venue for meeting")
+    date = fields.Date(string="Meeting date", help="Date of meeting")
+    time = fields.Float(string="Meeting time")
+    performing_operation_id = fields.Many2one(
+        comodel_name='performing.operation',
+        string='Performing Op',
+        group_expand='_read_group_performing_operation_ids',)
+    # performing_operation_adress = fields.Many2one(
+    #     comodel_name='performing.operation',
+    #     name = 'partner_ids.display_name',
+    #     string='Performing ADDRESS', )
+    performing_operation_adress = fields.Many2one(
+        comodel_name ='res.partner',
+        name='Adress', string='Performing Adress') 
+    #performing_operation_ka_nr = fields.Integer(related="performing_operation_id.ka_nr", readonly=False, string='Performing DD',)
+    #performing_operation_name = fields.Char(related="performing_operation_id.partner_ids.", string='Performing DD',)
+
 
     @api.multi
     def action_send_eletter(self):
         template_id = self.env.ref('mail_outplacement_report.email_template_assigned_coach').id
         _logger.warn("ALDIN: template_id1: %s" % template_id)
         template = self.env['mail.template'].browse(template_id)
+        
         _logger.warn("ALDIN: template: %s" % template)
         _logger.warn("ALDIN: self: %s" % self)
         result = template.send_mail(self.id, force_send=True)
         _logger.warn("ALDIN: result: %s" % result)
-
+        
+        
