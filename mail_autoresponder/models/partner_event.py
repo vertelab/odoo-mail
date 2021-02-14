8f03db2311de8bc4924b93af8a9b35ea13ab0cdb
Miracle Ayodele <miracleaayodele@gmail.com>
2021-02-14 13:37:09 +0100

    modification to mail autoresponder

--- mail_autoresponder/models/partner_event.py
+++ mail_autoresponder/models/partner_event.py
@@ -19,18 +19,13 @@ class PartnerEvent(models.Model):
     country_id = fields.Many2one('res.country', 'Country', related='address_id.country_id', store=True, readonly=False)
     category = fields.Char(string="Category")
     user_id = fields.Many2one('res.users', string="Responsible", default=lambda self: self.env.user.id)
-    date_begin = fields.Datetime(string="Start Date", required=True)
-    date_end = fields.Datetime(string="End Date", required=True)
+    date_begin = fields.Date(string="Start Date", required=True)
+    date_end = fields.Date(string="End Date", required=True)
+    creation_date = fields.Date(string="Creation Date", default=lambda self: self._context.get('date', fields.Date.context_today(self)))
 
     @api.model
     def _prefill_email_schedule_ids(self):
         values = [
-            (0, 0, {
-                'template_id': self.env.ref('mail_autoresponder.on_boarding_email_template'),
-                'interval_type': 'after_sub',
-                'interval_nbr': 1,
-                'interval_unit': 'now'
-            }),
             (0, 0, {
                 'template_id': self.env.ref('mail_autoresponder.meeting_reminder_email_template'),
                 'interval_type': 'before_event',
@@ -57,8 +52,6 @@ class PartnerEvent(models.Model):
         event_ids = self.env['partner.event'].search([('state', '=', 'running')])
         for record in event_ids:
             for rec in record.email_schedule_ids:
-                if rec.interval_type == 'after_sub' and not rec.sent:
-                    self.after_registration(record, rec)
                 if rec.interval_type == 'before_event' and not rec.sent:
                     self.before_event(record, rec)
                 if rec.interval_type == 'after_event' and not rec.sent:
@@ -66,14 +59,7 @@ class PartnerEvent(models.Model):
 
     def after_registration(self, event_id, event_mail_id):
         today_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        if event_mail_id.interval_unit == 'now':
-            self.email_to_contacts(event_id, event_mail_id)
-        elif event_mail_id.interval_unit == 'hours':
-            _datetime = event_id.create_date + datetime.timedelta(hours=event_mail_id.interval_nbr)
-            _date = _datetime.strftime("%Y-%m-%d %H")
-            if _date == datetime.datetime.today().strftime("%Y-%m-%d %H"):
-                self.email_to_contacts(event_id, event_mail_id)
-        elif event_mail_id.interval_unit == 'days':
+        if event_mail_id.interval_unit == 'days':
             _datetime = event_id.create_date + datetime.timedelta(days=event_mail_id.interval_nbr)
             _date = _datetime.strftime("%Y-%m-%d")
             if _date == today_date:
@@ -86,14 +72,7 @@ class PartnerEvent(models.Model):
 
     def before_event(self, event_id, event_mail_id):
         today_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        if event_mail_id.interval_unit == 'now':
-            self.email_to_contacts(event_id, event_mail_id)
-        elif event_mail_id.interval_unit == 'hours':
-            _datetime = event_id.date_begin - datetime.timedelta(hours=event_mail_id.interval_nbr)
-            _date = _datetime.strftime("%Y-%m-%d %H")
-            if _date == datetime.datetime.today().strftime("%Y-%m-%d %H"):
-                self.email_to_contacts(event_id, event_mail_id)
-        elif event_mail_id.interval_unit == 'days':
+        if event_mail_id.interval_unit == 'days':
             _datetime = event_id.date_begin - datetime.timedelta(days=event_mail_id.interval_nbr)
             _date = _datetime.strftime("%Y-%m-%d")
             if _date == today_date:
@@ -106,14 +85,7 @@ class PartnerEvent(models.Model):
 
     def after_event(self, event_id, event_mail_id):
         today_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        if event_mail_id.interval_unit == 'now':
-            self.email_to_contacts(event_id, event_mail_id)
-        elif event_mail_id.interval_unit == 'hours':
-            _datetime = event_id.date_end + datetime.timedelta(hours=event_mail_id.interval_nbr)
-            _date = _datetime.strftime("%Y-%m-%d %H")
-            if _date == datetime.datetime.today().strftime("%Y-%m-%d %H"):
-                self.email_to_contacts(event_id, event_mail_id)
-        elif event_mail_id.interval_unit == 'days':
+        if event_mail_id.interval_unit == 'days':
             _datetime = event_id.date_end + datetime.timedelta(days=event_mail_id.interval_nbr)
             _date = _datetime.strftime("%Y-%m-%d")
             if _date == today_date:
@@ -145,20 +117,20 @@ class EventEmailSchedule(models.Model):
     _name = 'partner.event.email.schedule'
 
     name = fields.Text(string='Description')
-    template_id = fields.Many2one('mail.template', string="Email Template")
+    template_id = fields.Many2one('mail.template', string="Email Template", domain=[('model', '=', 'partner.event')])
 
     partner_event_id = fields.Many2one('partner.event', string="Partner Event")
 
     interval_nbr = fields.Integer('Interval', default=1)
     interval_unit = fields.Selection([
-        ('now', 'Immediately'),
-        ('hours', 'Hour(s)'), ('days', 'Day(s)'),
-        ('weeks', 'Week(s)'), ('months', 'Month(s)')],
-        string='Unit', default='hours', required=True)
+        ('days', 'Day(s)'),
+        ('weeks', 'Week(s)')],
+        string='Unit', default='days', required=True)
     interval_type = fields.Selection([
-        ('after_sub', 'After each registration'),
         ('before_event', 'Before the event'),
         ('after_event', 'After the event')],
         string='Trigger ', default="before_event", required=True)
+
+    date = fields.Date(string="Date")
 
     sent = fields.Boolean('Sent', readonly=True)
