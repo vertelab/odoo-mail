60fa91d14b59c5f14f6c1af6254af22c2d61c5aa
Fredrik Arvas <fredrik@arvas.se>
2021-02-23 08:46:37 +0100

    Update __manifest__.py

--- send_mail_nadim/__manifest__.py
+++ send_mail_nadim/__manifest__.py
@@ -2,10 +2,11 @@
 
 {
     'name': 'Send mail Nadim',
-    'version': '1.0',
+    'version': '12.0.1.0.1',
     'category': 'other',
     'description': """
-Instead of SMTP, this module use RestAPI to send mail between Odoo server and Nadim integration server
+Instead of SMTP, this module use RestAPI to send mail between Odoo server and Nadim integration server\n
+v12.0.1.0.1 Updated code with fixes\n
 """,
     'depends': ['fetchmail'],
     'data': ['security/ir.model.access.csv',
