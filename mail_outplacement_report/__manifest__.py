1cdbb70d3f612af0f22b1252d99d5bb10f299b85
aldindrag <draal@89-21.wt.ams.se>
2021-03-09 15:23:23 +0100

    12.0-AFC-1614 - Upadated manifest

--- mail_outplacement_report/__manifest__.py
+++ mail_outplacement_report/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "DAFA Outplacement Templates",
-    "version": "12.0.2.0.1",
+    "version": "V12.0.1.0.3",
     "author": "Vertel AB",
     "license": "AGPL-3",
     "website": "https://vertel.se/",
