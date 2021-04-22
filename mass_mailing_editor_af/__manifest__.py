19094955d381eba16d433b5b8dfb181e10eb1f19
Fredrik Arvas <fredrik.arvas@vertel.se>
2021-04-22 16:02:16 +0200

    added mass_mailing_editor

---
+++ mass_mailing_editor_af/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "AF Mass Mailing Snippets",
+    "summary": "This version is used to customize the Odoo standard mail body widgets in accordance to 'Af' mail template.",
+    "version": "12.0.0.0.1",
+    "category": "Email Marketing",
+    "description": """
+	 v12.0.0.1 AFC-2067  Added snippets in Mass mailing body.\n
+    """,
+    "license": "AGPL-3",
+    "maintainer": "Vertel AB",
+    "author": "Vertel AB",
+    "contributors": ["Vertel AB"],
+    "depends": [
+        "mass_mailing"
+    ],
+    "data": [
+        'views/snippets_themes.xml'
+    ],
+    "auto_install": False,
+    "installable": True,
+}
