9b0a00e7d5279f2ae611f8d274e15195a1b1015b
Fredrik Arvas <fredrik.arvas@vertel.se>
2021-04-24 10:15:10 +0200

    Grouped Af Snippets, added unsubscribe link + typos

--- mass_mailing_editor_af/__manifest__.py
+++ mass_mailing_editor_af/__manifest__.py
@@ -1,10 +1,11 @@
 {
     "name": "AF Mass Mailing Snippets",
     "summary": "This version is used to customize the Odoo standard mail body widgets in accordance to 'Af' mail template.",
-    "version": "12.0.0.0.1",
+    "version": "12.0.0.0.2",
     "category": "Email Marketing",
     "description": """
 	 v12.0.0.1 AFC-2067  Added snippets in Mass mailing body.\n
+	 v12.0.0.2 AFC-2067 IMP  Added snippets in Mass mailing body.\n
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
@@ -20,3 +21,4 @@
     "auto_install": False,
     "installable": True,
 }
+
