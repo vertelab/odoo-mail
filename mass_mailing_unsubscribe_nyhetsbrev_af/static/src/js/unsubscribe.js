5bca71223612209df9ead308341ce4120083fb81
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2021-05-12 13:37:02 +0530

    [IMP] mass_mailing_unsubscribe_nyhetsbrev_af: AFC-2173 Fixed setting menu issue on debug mode.

--- mass_mailing_unsubscribe_nyhetsbrev_af/static/src/js/unsubscribe.js
+++ mass_mailing_unsubscribe_nyhetsbrev_af/static/src/js/unsubscribe.js
@@ -1,4 +1,4 @@
-odoo.define('mass_mailing_custom_unsubscribe.unsubscribe', function (require) {
+odoo.define('mass_mailing_unsubscribe_nyhetsbrev_af.unsubscribe', function (require) {
     'use strict';
     var ajax = require('web.ajax');
     var core = require('web.core');
