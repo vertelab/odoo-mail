e2ab20034c0113c81d651fec7f5222b34ef6192c
Fredrik Arvas <fredrik.arvas@vertel.se>
2021-05-23 09:54:29 +0200

    Fixed number and manifest

--- mail_partner_messages/__manifest__.py
+++ mail_partner_messages/__manifest__.py
@@ -1,12 +1,13 @@
 {
-    "name": "AF Mail Partner Messages",
-    "summary": "AF Mail Client Contact",
-    "version": "12.0.0.2",
+    "name": "Mail Partner Messages",
+    "summary": "Mail Client Contact",
+    "version": "12.0.1.0.3",
     "category": "Messaging",
 	"description": """
 	 v12.0.0.1 AFC-1313 Mail Client Odoo Messaging.
-	 v12.0.0.2 AFC-2322 Added new tab inside Contact & Jobseeker and new menu 'Messages' for Mass mailing messages for 
-	    Newsletter >> Manual user group.
+	 v12.0.0.2 AFC-2322 Added new tab inside Contact & Jobseeker and new menu 'Messages' for Mass mailing messages for Newsletter >> Manual user group.
+	 v12.0.1.0.3 Fixed numbering and Name in manifest
+	 
 	     """,
     "license": "AGPL-3",
     "author": "Vertel AB",
