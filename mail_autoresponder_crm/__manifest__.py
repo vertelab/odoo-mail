e015bc2470c43b608548a4909ea7debca399b779
Fredrik Arvas <fredrik.arvas@vertel.se>
2021-05-23 09:52:49 +0200

    AFC-2322 Corrected Menu-groups

--- mail_autoresponder_crm/__manifest__.py
+++ mail_autoresponder_crm/__manifest__.py
@@ -3,10 +3,13 @@
 {
     'name': 'Mail Autoresponder CRM',
     'summary': "Adds an automatic emailing trigger based service",
-    'version': '12.0.0.0.3',
+    'version': '12.0.1.0.4',
     'author': 'Vertel AB',
     'license': 'AGPL-3',
     'maintainer': 'Vertel AB',
+    'description': """
+        - 12.0.1.0.4  Changed user-groups to menu \n
+    """,
     'website': 'http://www.vertel.se',
     'depends': ['base_setup', 'mail', 'mail_autoresponder', 'partner_daily_notes', 'af_security'
                 ],
