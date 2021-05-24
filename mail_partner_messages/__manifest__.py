25d6adcb05341f7449fdd6b55d2e971d3420bfcb
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2021-05-24 16:21:37 +0530

    [ADD] AFC-2322: Updated manifest file.

--- mail_partner_messages/__manifest__.py
+++ mail_partner_messages/__manifest__.py
@@ -1,12 +1,13 @@
 {
     "name": "Mail Partner Messages",
     "summary": "Mail Client Contact",
-    "version": "12.0.1.0.3",
+    "version": "12.0.1.0.4",
     "category": "Messaging",
 	"description": """
 	 v12.0.0.1 AFC-1313 Mail Client Odoo Messaging.
 	 v12.0.0.2 AFC-2322 Added new tab inside Contact & Jobseeker and new menu 'Messages' for Mass mailing messages for Newsletter >> Manual user group.
 	 v12.0.1.0.3 Fixed numbering and Name in manifest
+	 v12.0.1.0.4 AFC-2322 Added Email button on Message to redirect Email, Added statistics on Messages, Set 'Keep Archive' default true and required.
 	 
 	     """,
     "license": "AGPL-3",
