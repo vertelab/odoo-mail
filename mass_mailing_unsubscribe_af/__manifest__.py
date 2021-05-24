dd194a4a56ddbf3599a0eacf6f64c8b38dc4a995
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2021-05-24 16:15:59 +0530

    [ADD] AFC-2341: mass_mailing_unsubscribe_af: Changed module name mass_mailing_unsubscribe_af from mass_mailing_unsubscribe_nyhetsbrev_af, mail_autoresponder_crm: Prevented Email Marketing group who has no Newsletter Manual group access.

--- mass_mailing_unsubscribe_af/__manifest__.py
+++ mass_mailing_unsubscribe_af/__manifest__.py
@@ -1,11 +1,12 @@
 {
-    "name": "Mass mailing unsubscribe nyhetsbrev AF",
+    "name": "Mass mailing unsubscribe AF",
     "summary": "Update layout of unsubscribe page.",
-    "version": "12.0.0.0.2",
+    "version": "12.0.0.0.3",
     "category": "Email Marketing",
     "description": """
 	 v12.0.0.1 AFC-2173  Update layout of Unsubscribe Page.\n
 	 v12.0.0.2 AFC-2173 Fixed bug with link.\n
+	 v12.0.0.3 AFC-2341 Changed module name from mass_mailing_unsubscribe_nyhetsbrev_af to mass_mailing_unsubscribe_af.\n
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
