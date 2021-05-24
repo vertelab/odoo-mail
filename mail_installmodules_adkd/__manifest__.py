eb410152a66ffe4f967741bb972b0747ca4df318
Fredrik Arvas, Vertel AB <61729317+fredrikarvas-vertel@users.noreply.github.com>
2021-05-24 21:30:36 +0200

    added mail_partner_message and unsubscribe-modules

--- mail_installmodules_adkd/__manifest__.py
+++ mail_installmodules_adkd/__manifest__.py
@@ -6,9 +6,10 @@
     "author": "Swedish Public Employement Service",
     "license": "AGPL-3",
     "website": "https://arbetsformedlingen.se/",
+    "description": "This module is maintained here https://github.com/vertelab/odoo-mail/tree/Dev-12.0/mass_mailing_unsubscribe_af",
     "category": "Tools",
     "depends": [
-		"partner_mail_client",		# odoo-base This modules adds menus and tabs to display the contacts messages
+   	        # "partner_mail_client",		# odoo-base This modules adds menus and tabs to display the contacts messages. Is replaced by mail_partner_messages
 		"mass_mailing",			# Odoo module for newsletters
 		"mail_autoresponder",		# generic module to process automatic newsletters
 		"mail_autoresponder_crm",	# CRM-addon to glue the module to CRM-specific views. (This depends on odoo-base/partner_daily_notes which depends onodoo-base/partner_view_360)
@@ -18,6 +19,8 @@
 		"mass_mailing_editor_af",		# odoo-mail This adds custom snippets to the mail-editor
 		"mail_mass_mailing_gdrp_limitations",	# Module to hide personal-data in the Display Sample in MassMailing and Autoresponder-modules
 		"mail_massmailing_statistics_af",	# odoo-mail This extends the statistics displayed in smart buttons to give percentages.
+	    	"mass_mailing_unsubscribe_af",		# Fixes layout of unsubscribe-page https://github.com/vertelab/odoo-mail/tree/Dev-12.0/mass_mailing_unsubscribe_af
+		"mail_partner_messages",		# Adds a tab with messages to the contact and jobseeker view. https://github.com/vertelab/odoo-mail/tree/Dev-12.0/mail_partner_messages
 
 
 			],
