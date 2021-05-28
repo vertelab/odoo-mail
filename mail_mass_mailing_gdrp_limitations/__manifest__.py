1352b2f716c93cf357ce28b3e8a5c77c4117e153
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2021-05-28 15:23:51 +0530

    [ADD] Updated manifest file.

--- mail_mass_mailing_gdrp_limitations/__manifest__.py
+++ mail_mass_mailing_gdrp_limitations/__manifest__.py
@@ -1,10 +1,11 @@
 {
     "name": "AF Mass Mailing gdpr limitations",
     "summary": "This module limits the disclosure of personal data in the mass-mailing module",
-    "version": "12.0.0.0.1",
+    "version": "12.0.0.0.2",
     "category": "Email Marketing",
     "description": """
 	 12.0.0.0.1  AFC-2167 Updated list view of contacts from Email marketing smart buttons.
+	 12.0.0.0.2  AFC-2186 Added new fields on list view of contacts from Email marketing smart buttons.
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
