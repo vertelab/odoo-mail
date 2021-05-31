d1ef43be1916f6c4a2dc116d54ad9c08a2e5b110
Fredrik Arvas <fredrik.arvas@vertel.se>
2021-05-31 18:19:13 +0200

    Re added the SSYK-codes

--- mail_mass_mailing_gdrp_limitations/__manifest__.py
+++ mail_mass_mailing_gdrp_limitations/__manifest__.py
@@ -1,11 +1,12 @@
 {
     "name": "AF Mass Mailing gdpr limitations",
     "summary": "This module limits the disclosure of personal data in the mass-mailing module",
-    "version": "12.0.0.0.2",
+    "version": "12.0.1.1.0",
     "category": "Email Marketing",
     "description": """
 	 12.0.0.0.1  AFC-2167 Updated list view of contacts from Email marketing smart buttons.
 	 12.0.0.0.2  AFC-2186 Added new fields on list view of contacts from Email marketing smart buttons.
+	 12.0.1.1.0  AFC-2186 re-added the SSYK-codes.
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
