33e462385a2039d44b35468707d8adfedeb01189
nils-vertel <72382478+nils-vertel@users.noreply.github.com>
2021-07-14 09:40:07 +0200

    AFC-2589-mass_mailing_unsubscribe_af-improvements
    * Refactoring of code
    * Fixed timezone bug

--- mass_mailing_unsubscribe_af/wizard/import_deregistration.py
+++ mass_mailing_unsubscribe_af/wizard/import_deregistration.py
@@ -2,6 +2,7 @@ import base64
 from datetime import datetime, timedelta
 import csv
 import io
+import logging
 
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
@@ -10,8 +11,10 @@ from odoo.modules.module import get_resource_path
 import pytz
 from xlrd import open_workbook
 
+_logger = logging.getLogger(__name__)
 
-class ImportDeregistratioFile(models.TransientModel):
+
+class ImportDeregistrationFile(models.TransientModel):
     _name = 'import.deregistration.file'
     _description = 'Import Deregistration File'
 
@@ -24,204 +27,117 @@ class ImportDeregistratioFile(models.TransientModel):
     dwnld_xls_file = fields.Binary("XLS File")
     dwnld_txt_file = fields.Binary("TXT File")
 
-    def adjust_for_tz(self, date, from_tz='Europe/Stockholm', to_tz='utc'):
+    @staticmethod
+    def adjust_tz(date, from_tz='Europe/Stockholm', to_tz='utc'):
+        """Change between timezones."""
+        if isinstance(date, str):
+            date = datetime.strptime(date, '%Y-%m-%d %H:%M')
+        to_tz = pytz.timezone(to_tz)
         return pytz.timezone(from_tz).localize(date).astimezone(to_tz).replace(tzinfo=None)
 
+    @staticmethod
+    def check_header(header):
+        """
+        Verify that header has the required columns as well as create
+         a dict with indexes.
+         """
+        # Verify Header, Force it lowercase
+        header = {x.lower(): index for index, x in enumerate(header)}
+        correct_header = ['e-postadress', 'opt out date', 'reason']
+        if not all(item in header for item in correct_header):
+            raise UserError(_('Please correct Header in file!\n'
+                              'Header has to contain:\n') +
+                            '\n'.join(correct_header))
+        return header
+
+    def import_row(self, email, date, reason_val, unsub_obj, mail_list_obj,
+                   black_list_obj, partner_obj, reason_obj):
+        """Handle an individual row."""
+        date = self.adjust_tz(date)
+        reason = reason_obj.search([('name', '=', reason_val)])
+        details = ''
+        if not reason:
+            reason = self.env.ref('mass_mailing_custom_unsubscribe.reason_other')
+            details = reason_val if reason_val else 'From Deregistration List'
+        if email:
+            contact_list = mail_list_obj.search([('email', '=', email)])
+            for contact in contact_list:
+                unsub_obj.create({
+                        'date': date,
+                        'mailing_list_ids':
+                            [(4, lst.list_id.id) for lst in
+                             contact.subscription_list_ids],
+                        'email': email,
+                        'action': 'unsubscription',
+                        'reason_id': reason.id if reason else '',
+                        'details': details,
+                })
+                for mail_list in contact.subscription_list_ids:
+                    mail_list.opt_out = True
+            if not contact_list:
+                partners = partner_obj.search([('email', '=', email)])
+                for partner in partners:
+                    unsub_obj.create({
+                            'date': date,
+                            'email': email,
+                            'action': 'unsubscription',
+                            'reason_id': reason.id if reason else '',
+                            'details': details,
+                            'unsubscriber_id': 'res.partner,' + str(partner.id)
+                    })
+                    if not black_list_obj.search([('email', '=', email)]):
+                        black_list_obj.create({'email': email})
+
     def import_data(self):
+        """Parse and import a file."""
         if not self.file or not \
                 self.filename.lower().endswith(('.xls', '.xlsx', '.csv', 'txt')):
             raise UserError(_("Please Select an .xls, .xlsx, .txt or .csv file to Import."))
-
         unsub_obj = self.env['mail.unsubscription']
         mail_list_obj = self.env['mail.mass_mailing.contact']
         black_list_obj = self.env['mail.blacklist']
         partner_obj = self.env['res.partner']
         reason_obj = self.env['mail.unsubscription.reason']
-
         now = datetime.now()
-        if self.filename.lower().endswith('.csv'):
-            data_list = []
-            headers = []
-            result_vals = []
 
-            # Decode data to string
+        if self.filename.lower().endswith(('.csv', '.txt')):
+            # Decode data to string, Odoo supplies it as base64
             csv_data = base64.b64decode(self.file)
             data_file = io.StringIO(csv_data.decode("utf-8"))
             # Read CSV
             headers, *data = csv.reader(data_file, delimiter=',')
-            _logger.warning(headers)
-            _logger.warning(data)
-            # Put rows to data list
-            data_list.extend(csv_reader)
-            # Set headers
-            headers.extend(data_list[0])
 
-            _logger.warning(headers)
-            _logger.warning(data_list)
+            # Verify Header, Force it lowercase and make a dict
+            headers = self.check_header(headers)
+            for row in data:
+                try:
+                    email = row[headers['e-postadress']]
+                    date = row[headers['opt out date']] or now
+                    reason_val = row[headers['reason']]
+                except IndexError:
+                    # Empty or malformed row.
+                    _logger.warning(f'Could not parse the row: {row}')
+                else:
+                    self.import_row(email, date, reason_val, unsub_obj,
+                                    mail_list_obj,black_list_obj, partner_obj,
+                                    reason_obj)
 
-            # Prepare values
-            for row in data_list[1:]:
-                result_dict = {}
-                counter = 0
-                for cell in row:
-                    result_dict.update({headers[counter]: cell})
-                    counter += 1
-                result_vals.append(result_dict)
-            correct_header = ['E-postadress', 'Opt out date', 'Reason']
-            check_header = all(item in headers for item in correct_header)
-            if not check_header:
-                raise UserError(_("Please correct Header in file!"))
-            else:
-                for row in result_vals:
-                    email = row.get('E-postadress')
-                    date = row.get('Opt out date')
-                    reason_val = row.get('Reason')
-                    if date:
-                        date = str(datetime.strptime(str(date), '%Y-%m-%d %H:%M') - timedelta(hours=2))
-                    reason = reason_obj.search([('name', '=', reason_val)])
-                    details = ''
-                    if not reason:
-                        reason = self.env.ref('mass_mailing_custom_unsubscribe.reason_other')
-                        details = reason_val if reason_val else 'From Deregistration List'
-                    if email:
-                        contact_list = mail_list_obj.search([('email', '=', email)])
-                        for contact in contact_list:
-                            unsub_obj.create({
-                                'date': date if date else now,
-                                'mailing_list_ids': [(4, list.list_id.id) for list in contact.subscription_list_ids],
-                                'email': email,
-                                'action': 'unsubscription',
-                                'reason_id': reason.id if reason else False,
-                                'details': details,
-                            })
-                            for mail_list in contact.subscription_list_ids:
-                                mail_list.opt_out = True
-                        if not contact_list:
-                            partners = partner_obj.search([('email', '=', email)])
-                            for partner in partners:
-                                unsub_obj.create({
-                                    'date': date if date else now,
-                                    'email': email,
-                                    'action': 'unsubscription',
-                                    'reason_id': reason.id if reason else '',
-                                    'details': details,
-                                    'unsubscriber_id': 'res.partner,' + str(partner.id)
-                                })
-                                black_list = black_list_obj.search([('email', '=', email)])
-                                if not black_list:
-                                    black_list_obj.create({'email': email})
-        elif self.filename.lower().endswith('.txt'):
-            file_data = base64.decodestring(self.file)
-            file_data = file_data.decode("utf-8")
-            data_list = file_data.split('\n')
-            for data in data_list[1:]:
-                if data:
-                    row = data.split(',')
-                    if len(row) < 3:
-                        raise UserError(_("Correct following line! \n %s" % row))
-                    else:
-                        email = row[1]
-                        date = row[2]
-                        reason_val = row[3]
-                        if date:
-                            date = str(datetime.strptime(str(date), '%Y-%m-%d %H:%M') - timedelta(hours=2))
-                        if not isinstance(date, str):
-                            raise UserError(_("Format Date column with String!"))
-                        reason = reason_obj.search([('name', '=', reason_val)])
-                        details = ''
-                        if not reason:
-                            reason = self.env.ref('mass_mailing_custom_unsubscribe.reason_other')
-                            details = reason_val if reason_val else 'From Deregistration List'
-                        if email:
-                            contact_list = mail_list_obj.search([('email', '=', email)])
-                            for contact in contact_list:
-                                unsub_obj.create({
-                                    'date': date if date else now,
-                                    'mailing_list_ids': [(4, list.list_id.id) for list in contact.subscription_list_ids],
-                                    'email': email,
-                                    'action': 'unsubscription',
-                                    'reason_id': reason.id if reason else False,
-                                    'details': details,
-                                })
-                                for mail_list in contact.subscription_list_ids:
-                                    mail_list.opt_out = True
-                            if not contact_list:
-                                partners = partner_obj.search([('email', '=', email)])
-                                for partner in partners:
-                                    unsub_obj.create({
-                                        'date': date if date else now,
-                                        'email': email,
-                                        'action': 'unsubscription',
-                                        'reason_id': reason.id if reason else '',
-                                        'details': details,
-                                        'unsubscriber_id': 'res.partner,' + str(partner.id)
-                                    })
-                                    black_list = black_list_obj.search([('email', '=', email)])
-                                    if not black_list:
-                                        black_list_obj.create({'email': email})
-        else:
+        elif self.filename.lower().endswith(('.xls', '.xlsx')):
             data = base64.decodestring(self.file)
             book = open_workbook(file_contents=data or b'')
             sheet = book.sheet_by_index(0)
 
-            headers = []
-            for rowx, row in enumerate(map(sheet.row, range(1)), 1):
-                for colx, cell in enumerate(row, 1):
-                    headers.append(cell.value)
-
-            correct_header = ['E-postadress', 'Opt out date', 'Reason']
-            check_header = all(item in headers for item in correct_header)
-            if not check_header or (len(headers) < 3) or (headers[1] != 'E-postadress') or \
-                    (headers[2]!= 'Opt out date') or (headers[3] != 'Reason'):
-                raise UserError(_("Please correct Header in File!"))
-
-            for rowx, row in enumerate(map(sheet.row, range(1, sheet.nrows)), 1):
-                email = ''
-                date = ''
-                reason_val = ''
-                for colx, cell in enumerate(row, 1):
-                    if colx == 2:
-                        email = cell.value
-                    elif colx == 3:
-                        date = cell.value
-                    elif colx == 4:
-                        reason_val = cell.value
-                if date:
-                    date = str(datetime.strptime(str(date), '%Y-%m-%d %H:%M') - timedelta(hours=2))
-                if not isinstance(date, str):
-                    raise UserError(_("Format Date column with String!"))
-                reason = reason_obj.search([('name', '=', reason_val)])
-                details = ''
-                if not reason:
-                    reason = self.env.ref('mass_mailing_custom_unsubscribe.reason_other')
-                    details = reason_val if reason_val else 'From Deregistration List'
-                if email:
-                    contact_list = mail_list_obj.search([('email', '=', email)])
-                    for contact in contact_list:
-                        unsub_obj.create({
-                            'date': date if date else now,
-                            'mailing_list_ids': [(4, list.list_id.id) for list in contact.subscription_list_ids],
-                            'email': email,
-                            'action': 'unsubscription',
-                            'reason_id': reason.id if reason else False,
-                            'details': details,
-                        })
-                        for mail_list in contact.subscription_list_ids:
-                            mail_list.opt_out = True
-                    if not contact_list:
-                        partners = partner_obj.search([('email', '=', email)])
-                        for partner in partners:
-                            unsub_obj.create({
-                                'date': date if date else now,
-                                'email': email,
-                                'action': 'unsubscription',
-                                'reason_id': reason.id if reason else '',
-                                'details': details,
-                                'unsubscriber_id': 'res.partner,' + str(partner.id)
-                            })
-                            black_list = black_list_obj.search([('email', '=', email)])
-                            if not black_list:
-                                black_list_obj.create({'email': email})
+            # Verify Header, Force it lowercase and make a dict
+            headers = self.check_header([cell.value for cell in sheet.row(0)])
+            for row_nr in range(sheet.nrows, 1):
+                email = sheet.cell_value(row_nr, headers['e-postadress'])
+                date = sheet.cell_value(row_nr, headers['opt out date'])
+                reason_val = sheet.cell_value(row_nr, headers['reason'])
+                self.import_row(email, date, reason_val, unsub_obj,
+                                mail_list_obj, black_list_obj, partner_obj,
+                                reason_obj)
+        else:
+            raise UserError(_('Unknown file ending: {}').format(self.filename))
 
     def download_xls_file(self):
         xls_file_path = get_resource_path(
@@ -282,4 +198,3 @@ class ImportDeregistratioFile(models.TransientModel):
             'target': 'new',
             'res_id': self.id,
         }
-
