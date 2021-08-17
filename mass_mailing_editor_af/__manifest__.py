bcc1b9c0c3a6abce95a40e802e5b059dc5fd02fc
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2021-08-17 11:12:03 +0530

    [ADD] mass_mailing_editor_af, mail_massmail_template_af: AFC-1725 Removed one sample template, Added dynamic links on sample template, Added new snippets, Updated some style on sample template.

--- mass_mailing_editor_af/__manifest__.py
+++ mass_mailing_editor_af/__manifest__.py
@@ -1,14 +1,15 @@
 {
     "name": "AF Mass Mailing Snippets",
     "summary": "This version is used to customize the Odoo standard mail body widgets in accordance to 'Af' mail template.",
-    "version": "12.0.0.0.5",
+    "version": "12.0.0.0.6",
     "category": "Email Marketing",
     "description": """
-	 v12.0.0.1 AFC-2067  Added snippets in Mass mailing body.\n
-	 v12.0.0.2 AFC-2067 IMP  Added snippets in Mass mailing body.\n
-	 v12.0.0.3 AFC-2067 IMP  Added snippets in separate section of Mass mailing body.\n
-	 v12.0.0.4 AFC-2067 Fix  Fixed setting menu URL issue.\n
-	 v12.0.0.5 AFC-2067 Fix  Fixed Font and Background issue in Email.\n
+    v12.0.0.1 AFC-2067  Added snippets in Mass mailing body.\n
+    v12.0.0.2 AFC-2067 IMP  Added snippets in Mass mailing body.\n
+    v12.0.0.3 AFC-2067 IMP  Added snippets in separate section of Mass mailing body.\n
+    v12.0.0.4 AFC-2067 Fix  Fixed setting menu URL issue.\n
+    v12.0.0.5 AFC-2067 Fix  Fixed Font and Background issue in Email.\n
+    v12.0.0.6 AFC-1725 Added some more snippets in Mass mailing body\n
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
@@ -24,4 +25,3 @@
     "auto_install": False,
     "installable": True,
 }
-
