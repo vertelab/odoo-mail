771cf0b6a9bd6fb603f2058088df354f5d1e0406
Hannah Bergenroth <hannah.bergenroth@arbetsformedlingen.se>
2021-10-01 09:24:46 +0200

    update manifest

--- mass_mailing_autoresponder_crm/__manifest__.py
+++ mass_mailing_autoresponder_crm/__manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Mail Autoresponder CRM',
     'summary': "Adds an automatic emailing trigger based service",
-    'version': '12.0.1.0.6',
+    'version': '12.0.1.0.7',
     'author': 'Vertel AB',
     'license': 'AGPL-3',
     'maintainer': 'Vertel AB',
@@ -12,6 +12,7 @@
         - 12.0.1.0.5  AFC-2341 Added user-role to the 'Email Marketing'' to prevent it to be visible for users outside 
         of the Newsletter-Manual-group\n
         - 12.0.1.0.6  AFC-2117 Added new model for adkd campaign, added m2m adkd campaign field to res_partner \n
+        - 12.0.1.0.7 AFC-2817 Removed incorrect linebreak \n 
     """,
     'website': 'http://www.vertel.se',
     'depends': ['base_setup', 'mail', 'mass_mailing_autoresponder', 'partner_daily_notes', 'af_security'
