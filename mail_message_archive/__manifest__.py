f3dc93f6c192d711a8234069dd17e4236a92ea32
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-10-19 09:25:55 +0200

    Create __manifest__.py

---
+++ mail_message_archive/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021 Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Mail Message Archive',
+    'version': '14.0.1',
+    'category': 'other',
+    'license': 'AGPL-3',
+    'summary': 'Integrates Jitsi screen sharing to Odoo discussions.',
+    'description': """ """,
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se',
+    'depends': [
+        #'mail',
+    ],
+    'data': [
+        #'mail_templates.xml',
+    ],
+    'qweb': [
+        #'static/src/xml/composer.xml',
+    ],
+    'auto_install': True,
+}
+# vim:expandtab:smartindent:tabstop=4s:softtabstop=4:shiftwidth=4:
