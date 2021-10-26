cc250cb5f3619811395dd3bf0b35ade548a9913a
DavidLarsson90 <david.larsson@vertel.se>
2021-10-26 11:55:44 +0200

    mass_mailing_unsubscribe_af AFC-2890 Add email to blacklist also

--- mass_mailing_unsubscribe_af/__manifest__.py
+++ mass_mailing_unsubscribe_af/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Mass mailing unsubscribe AF",
     "summary": "Update layout of unsubscribe page.",
-    "version": "12.0.1.2.4",
+    "version": "12.0.1.3.0",
     "category": "Email Marketing",
     "description": """
 	 v12.0.0.0.1 AFC-2173 Update layout of Unsubscribe Page.\n
@@ -17,6 +17,7 @@
      v12.0.1.2.2 AFC-2802 Small bugfix in import.\n
      v12.0.1.2.3 AFC-2851 Added proper date handling for opt out date in xls files.\n
      v12.0.1.2.4 AFC-2820 Add translation to menu items.\n
+     v12.0.1.3.0 AFC-2890 Add email to blacklist also.\n
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
