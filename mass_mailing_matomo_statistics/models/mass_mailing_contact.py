de845283c696fada3bd2c67783e93e31a6248d9b
Haze Lidman <63632561+HazeLidman@users.noreply.github.com>
2021-10-28 15:50:40 +0200

    Update mass_mailing_contact.py

--- mass_mailing_matomo_statistics/models/mass_mailing_contact.py
+++ mass_mailing_matomo_statistics/models/mass_mailing_contact.py
@@ -5,6 +5,7 @@ import uuid
 import re
 import logging
 _logger = logging.getLogger(__name__)
+
 class MassMailingContact(models.Model):
     _inherit = 'mail.mass_mailing.contact'
     def opt_out_all(self, name):
