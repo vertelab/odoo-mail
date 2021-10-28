4481552ccce9d472c6b4f3073dda1e8a0b0447b6
Anders Wallenquist <anders.wallenquist@arbetsformedlingen.se>
2021-10-28 15:30:33 +0200

    12.0-CRM-AFC-2984-User-Mail * Added a module that adds a "opt out all" button on a mass_mailing form   view, which opts out that conntact from all mail_lists.

--- mass_mailing_matomo_statistics/__manifest__.py
+++ mass_mailing_matomo_statistics/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Mass mailing Matomo statistics AF",
     "summary": "This module will add snippet in Mass Mailing mail body to add Matomo image for email tracking.",
-    "version": "12.0.0.0.1",
+    "version": "12.0.0.0.2",
     "category": "Email Marketing",
     "description": """
     After installation of this module, Go to Setting >> Technical >> Parameter >> System Parameter. \n
@@ -10,6 +10,7 @@
     2. Search key 'mass_mailing_matomo_statistics.website_id' for Website ID. Update value with Matomo website ID.  \n\n
     
 	 v12.0.0.1 AFC-2100c  Added snippet in Mass Mailing mail body to add Matomo image for email tracking.\n
+	 v12.1.0.2 AFC-2984. Added a opt out all button on the mail.mass_mailing.contact form\n
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
@@ -19,6 +20,7 @@
         "mass_mailing"
     ],
     "data": [
+        'views/mass_mailing_views.xml',
         'views/snippets.xml',
         'data/ir.config_parameter.csv'
     ],
