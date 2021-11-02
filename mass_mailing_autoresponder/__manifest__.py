b79ec11215b53f8b4507a567890a3890cc1cd39a
Haze Lidman <haze.lidman@arbetsformedlingen.se>
2021-11-02 16:15:03 +0100

    mass_mailing_autoresponder: adjusted access right for ADKD manuel user mass_mailing_autoresponder: updated version

--- mass_mailing_autoresponder/__manifest__.py
+++ mass_mailing_autoresponder/__manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Mail Autoresponder',
     'summary': "Adds an automatic Emailing Trigger based service",
-    'version': '12.0.2.3.0',
+    'version': '12.0.2.3.1',
     'author': 'Vertel AB',
     'license': 'AGPL-3',
     'maintainer': 'Vertel AB',
@@ -21,6 +21,7 @@
         v12.0.2.2.0  Added weekdays and holidays filtering \n
         v12.0.2.2.1  Add translation to template name in autoresponder \n
         v12.0.2.3.0  AFC-2732 Adjust access right for email marketing user \n 
+        v12.0.2.3.1  AFC-3027 Adjust access right for ADKD manuel user \n 
     """,
     'depends': [
         'base_setup', 'mail', 'mass_mailing', 'resource',
