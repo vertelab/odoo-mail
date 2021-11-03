3db1a677ba28a5b520b43587d9f2c0d46e588f2d
DavidLarsson90 <79191629+DavidLarsson90@users.noreply.github.com>
2021-11-03 14:02:45 +0100

    12.0 afc 2117 2 one list (#108)
    * mass_mailing_import_mailing_list AFC-2968 New module for importing mailing list

---
+++ mass_mailing_import_mailing_list/models/mass_mailing.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution, third party addon
+#    Copyright (C) 2004-2021 Vertel AB (<http://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from odoo import api, models, fields
+
+
+class MailMassMailing(models.Model):
+    _inherit = "mail.mass_mailing"
+
+    def update_opt_out(self, email, list_ids, value):
+        """Save unsubscription reason when opting out from mailing."""
+        # Check if list has ADKd parent then use that list instead.
+        mail_list = self.env['mail.mass_mailing.list']
+        if len(list_ids) == 1:
+            mail_list = mail_list.search([('id', '=', list_ids[0])])
+        if (
+                mail_list and
+                mail_list.parent_id and
+                mail_list.parent_id.list_type in ('adkd', 'micro')
+        ):
+            list_ids[0] = mail_list.parent_id.id
+        return super().update_opt_out(email, list_ids, value)
