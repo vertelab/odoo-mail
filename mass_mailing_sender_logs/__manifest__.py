aea993161c280f7507f01faf8d67b2cac3317ac7
Nils Nyman-Waara <72382478+nils-vertel@users.noreply.github.com>
2021-11-10 16:41:36 +0100

    Update __manifest__.py

--- mass_mailing_sender_logs/__manifest__.py
+++ mass_mailing_sender_logs/__manifest__.py
@@ -2,13 +2,13 @@
 {
     "name": "AF Mass Mailing Sender Logs",
     "summary": "This module adds logger in Email Marketing >> Mailing.",
-    "version": "12.0.1.0.4",
+    "version": "12.0.1.1.0",
     "category": "Email Marketing",
     "description": """
 	 v12.0.0.1 AFC-2185 Added logger in Email Marketing >> Mailing.\n
 	 v12.0.1.0.2 AFC-2185 Updated code for Logs.\n
 	 v12.0.1.0.3 AFC-2864 Exposed the mass_mailing_id field to the link.tracker form view in order to make it easier to connect a tracked link with a mass mailing.\n
-	 v12.0.1.0.4 AFC-2864 Added so that the tracked links are able to track more that one click per user per link. Also added so that we can link a click to a user.\n
+	 v12.0.1.1.0 AFC-2864 Added so that the tracked links are able to track more that one click per user per link. Also added so that we can link a click to a user.\n
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
