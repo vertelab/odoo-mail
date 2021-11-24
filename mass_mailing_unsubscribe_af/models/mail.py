b7a0d3636d24cfbf23439e3a500cfbad07d01506
Nils Nyman-Waara <72382478+nils-vertel@users.noreply.github.com>
2021-11-24 09:28:09 +0100

    12.0-AFC-3111-Do-not-access-secrets (#129)

--- mass_mailing_unsubscribe_af/models/mail.py
+++ mass_mailing_unsubscribe_af/models/mail.py
@@ -9,3 +9,8 @@ class MailUnsubscription(models.Model):
         "Mass mailing",
         required=False,
         help="Mass mailing from which he was unsubscribed.")
+
+    customer_id = fields.Char(help="Customer reference ID.", compute='_compute_customer_id')
+    def _compute_customer_id(self):
+        for rec in self:
+            rec.customer_id = getattr(rec.sudo().unsubscriber_id, 'customer_id', '')
