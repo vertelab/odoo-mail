693df707abb4bc6e2c966270b67c916a08d51f8e
DavidLarsson90 <79191629+DavidLarsson90@users.noreply.github.com>
2021-11-29 11:34:30 +0100

    mass_mailing_unsubscribe_af afc-3124 Changed from email to sökande id… (#130)
    * Changed from email to sökande id for matching unsubscribe contact.
    * Changed to correct delimiter for csv.
    * .txt delimiter support.

--- mass_mailing_unsubscribe_af/__manifest__.py
+++ mass_mailing_unsubscribe_af/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Mass mailing unsubscribe AF",
     "summary": "Update layout of unsubscribe page.",
-    "version": "12.0.1.4.0",
+    "version": "12.0.1.5.0",
     "category": "Email Marketing",
     "description": """
 	 v12.0.0.0.1 AFC-2173 Update layout of Unsubscribe Page.\n
@@ -23,6 +23,7 @@
      v12.0.1.3.1 AFC-3112 Buggfix, remove debuging raise.\n
      v12.0.1.3.2 AFC-3111 Buggfix, Do not access forbidden data.\n
      v12.0.1.4.0 AFC-3028 Created new access group ADKD manual guest and adjusted access right \n
+     v12.0.1.5.0 AFC-3124 Changed from email to sökande id for matching unsubscribe contact \n
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
