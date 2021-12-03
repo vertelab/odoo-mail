7bb0bf20358ff5964deabf0c3dae4800b301cd33
Haze Lidman <63632561+HazeLidman@users.noreply.github.com>
2021-12-03 13:25:11 +0100

    mass_mailing_autoresponder: hide buttons for ADKD guest group (#135)
    mass_mailing_autoresponder: updated version
    mass_mailing_import_mailing_list: hide menu for ADKD guest group
    mass_mailing_import_mailing_list: upddated version
    
    Co-authored-by: Haze Lidman <haze.lidman@arbetsformedlingen.se>

--- mass_mailing_import_mailing_list/__manifest__.py
+++ mass_mailing_import_mailing_list/__manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Import mailing list',
     'summary': "Import mailing list for mass mailing",
-    'version': '12.0.1.0.3',
+    'version': '12.0.1.0.4',
     'author': 'Vertel AB',
     'license': 'AGPL-3',
     'maintainer': 'Vertel AB',
@@ -12,6 +12,7 @@
         - 12.0.1.0.1  AFC-2874 Some error handling \n
         - 12.0.1.0.2  AFC-3135 Bugfix, able to read .txt files again \n
         - 12.0.1.0.3  AFC-3135-2 Bugfix, changed to right index in filename split\n
+        - 12.0.1.0.4  AFC-3170 Hide import list menu for ADKD guest group\n
     """,
     'website': 'http://www.vertel.se',
     'depends': ['mass_mailing', 'mass_mailing_unsubscribe_af'],
