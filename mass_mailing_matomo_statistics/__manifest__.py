847f426e97835d6a7affcfb35cc0532832676cd0
Haze Lidman <haze.lidman@arbetsformedlingen.se>
2021-12-06 16:04:33 +0100

    mass_mailing_matomo_statistics: back code becasue pushed to wrong repo

--- mass_mailing_matomo_statistics/__manifest__.py
+++ mass_mailing_matomo_statistics/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Mass mailing Matomo statistics AF",
     "summary": "This module will add snippet in Mass Mailing mail body to add Matomo image for email tracking.",
-    "version": "12.0.0.1.0",
+    "version": "12.0.0.0.4",
     "category": "Email Marketing",
     "description": """
     After installation of this module, Go to Setting >> Technical >> Parameter >> System Parameter. \n
@@ -12,15 +12,14 @@
 	 v12.0.0.1 AFC-2100c  Added snippet in Mass Mailing mail body to add Matomo image for email tracking.\n
 	 v12.1.0.2 AFC-2984. Added a opt out all button on the mail.mass_mailing.contact form\n
 	 v12.1.0.3 AFC-3010. Fixade opt out bug in the core module\n
-     v12.0.0.4 AFC-2865 Added bouncing categories and a tree view to show them. There is a button when looking at bounces on a mass_mailing that will show the categories\n
-     v12.0.1.0 AFC-3207 hide opt out all button for ADKD guest group\n
+   v12.0.0.4 AFC-2865 Added bouncing categories and a tree view to show them. There is a button when looking at bounces on a mass_mailing that will show the categories\n
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
     "author": "Vertel AB",
     "contributors": ["Vertel AB"],
     "depends": [
-        "mass_mailing", "af_security"
+        "mass_mailing",
     ],
     "data": [
         'views/mass_mailing_views.xml',
