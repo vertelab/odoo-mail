786723fc375a8038cef5c0f27aec51c3105494a8
Haze Lidman <haze.lidman@arbetsformedlingen.se>
2021-12-07 11:23:51 +0100

    mass_mailing_count: Added translation file. mass_mailing_count: updated version. mass_mailing_count_af: Deleted repeated group name. mass_mailing_count_af: updated version

--- mass_mailing_count/__manifest__.py
+++ mass_mailing_count/__manifest__.py
@@ -1,11 +1,12 @@
 {
     "name": "Search Count Extension",
     "summary": "Adds method to extend search count with ability to search with higher rights.",
-    "version": "12.0.1.0.1",
+    "version": "12.0.1.0.2",
     "category": "Email Marketing",
     "description": """
-    1. 12.0.1.0.0 - AFC-2879 - Extend mass_mailing with Sudo search.
-    1. 12.0.1.0.1 - AFC-2879 - Bugfixes  
+    1. 12.0.1.0.0 - AFC-2879 - Extend mass_mailing with Sudo search.\n
+    1. 12.0.1.0.1 - AFC-2879 - Bugfixes.\n
+    1. 12.0.1.0.2 - AFC-3158 - Translated English to Swedish.\n 
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
