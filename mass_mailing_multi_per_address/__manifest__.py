0eddae60b0b417e3ce7adffb5022766d698d3105
Haze Lidman <haze.lidman@arbetsformedlingen.se>
2021-12-13 15:00:50 +0100

    mass_mailing_multi_per_address: updated version

--- mass_mailing_multi_per_address/__manifest__.py
+++ mass_mailing_multi_per_address/__manifest__.py
@@ -4,7 +4,7 @@
     "version": "12.0.1.0.1",
     "category": "Email Marketing",
     "description": """
-	 v12.0.0.1 AFC-3138 Fixed the case where odoo doesnt send mails to everyone who shares an address
+	    v12.0.0.1 AFC-3138 Fixed the case where odoo doesnt send mails to everyone who shares an address
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
