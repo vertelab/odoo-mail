c4467d69b4cded34bdb7388979f993ad297b94eb
Anders Wallenquist <anders.wallenquist@arbetsformedlingen.se>
2021-12-16 10:54:45 +0100

    AFC-2943 *
    Added clicks ratio and total clicks to the report, which can be seen on
    the privo view of mass_mailing reports.

--- mass_mailing_statistics_af/__manifest__.py
+++ mass_mailing_statistics_af/__manifest__.py
@@ -7,6 +7,8 @@
 	 v12.0.0.1 AFC-2100  Added numbers for email tracking events in Mass mailing kanban and form view.\n
 	 v12.1.0.0 AFC-2867  Added fields in mass mailing list view.\n
 	 v12.1.0.1 AFC-3042  Fixed the total click calulation
+	 v12.1.0.1 AFC-2943 Added clicks ratio to the report. Added to that we calulate the total number of clicks, so every
+	 time a link has been pressed in a mail, and added that to the report.
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
