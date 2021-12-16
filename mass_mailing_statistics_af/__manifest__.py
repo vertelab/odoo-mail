40ba78bb9d0a3e6524319bd5e54e0f7e4f4efe04
Anders Wallenquist <anders.wallenquist@arbetsformedlingen.se>
2021-12-16 10:59:23 +0100

    Forgot to change version number in the manifest file

--- mass_mailing_statistics_af/__manifest__.py
+++ mass_mailing_statistics_af/__manifest__.py
@@ -1,13 +1,13 @@
 {
     "name": "Mass mailing statistics AF",
     "summary": "This module will show number for Email tracking events in Mass Mailing.",
-    "version": "12.0.1.0.1",
+    "version": "12.0.1.0.2",
     "category": "Email Marketing",
     "description": """
 	 v12.0.0.1 AFC-2100  Added numbers for email tracking events in Mass mailing kanban and form view.\n
 	 v12.1.0.0 AFC-2867  Added fields in mass mailing list view.\n
 	 v12.1.0.1 AFC-3042  Fixed the total click calulation
-	 v12.1.0.1 AFC-2943 Added clicks ratio to the report. Added to that we calulate the total number of clicks, so every
+	 v12.1.0.2 AFC-2943 Added clicks ratio to the report. Added to that we calulate the total number of clicks, so every
 	 time a link has been pressed in a mail, and added that to the report.
     """,
     "license": "AGPL-3",
