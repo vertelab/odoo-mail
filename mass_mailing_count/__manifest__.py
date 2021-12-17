eb0c00310623b062c4da21def1c903cbf966b91d
Haze Lidman <haze.lidman@arbetsformedlingen.se>
2021-12-17 14:08:18 +0100

    mass_mailing_count: updated version mass_mailing_count_af: adjuested access right for guest group mass_mailing_count_af: updated version

--- mass_mailing_count/__manifest__.py
+++ mass_mailing_count/__manifest__.py
@@ -1,12 +1,13 @@
 {
     "name": "Search Count Extension",
     "summary": "Adds method to extend search count with ability to search with higher rights.",
-    "version": "12.0.1.0.2",
+    "version": "12.0.1.0.3",
     "category": "Email Marketing",
     "description": """
     1. 12.0.1.0.0 - AFC-2879 - Extend mass_mailing with Sudo search.\n
     1. 12.0.1.0.1 - AFC-2879 - Bugfixes.\n
     1. 12.0.1.0.2 - AFC-3158 - Translated English to Swedish.\n 
+    1. 12.0.1.0.3 - AFC-3259 - Fixed translation problem.\n 
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
