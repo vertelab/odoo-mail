d65be5fd3b99d401809e5921850f09ed230e8e3d
Nils Nyman-Waara <72382478+nils-vertel@users.noreply.github.com>
2022-01-13 07:31:05 +0100

    12.0-AFC-3136-Fix-unsibscribe-view
    * Fixed the unsubscribe form view so that unsubscriber_id is accessible.
    Co-authored-by: Anders Wallenquist <anders.wallenquist@arbetsformedlingen.se>

--- mass_mailing_unsubscribe_af/__manifest__.py
+++ mass_mailing_unsubscribe_af/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Mass mailing unsubscribe AF",
     "summary": "Update layout of unsubscribe page.",
-    "version": "12.0.1.6.2",
+    "version": "12.0.1.6.3",
     "category": "Email Marketing",
     "description": """
 	 v12.0.0.0.1 AFC-2173 Update layout of Unsubscribe Page.\n
@@ -27,6 +27,7 @@
      v12.0.1.6.0 AFC-3305 Changed from email to sökande id for matching blacklist contact \n
      v12.0.1.6.1 AFC-3179 Change border color of textarea in unsubscription form \n
      v12.0.1.6.2 AFC-3203 Added some js to make a popup when clicking on unsubscription button \n
+     v12.0.1.6.3 AFC-3136 Bugfix, can now access unsubscriber_id when creating unsubscription. \n
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
