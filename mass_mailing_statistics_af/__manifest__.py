ef3076c46bc7fe3465ce2548f7d76afffffbd2c3
Haze Lidman <63632561+HazeLidman@users.noreply.github.com>
2022-01-18 11:13:48 +0100

    mass_mailing_sender_logs: moved link tracker code to mass_mailing_statistics_af module. (#152)

--- mass_mailing_statistics_af/__manifest__.py
+++ mass_mailing_statistics_af/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Mass mailing statistics AF",
     "summary": "This module will show number for Email tracking events in Mass Mailing.",
-    "version": "12.0.1.0.2",
+    "version": "12.0.1.0.3",
     "category": "Email Marketing",
     "description": """
 	 v12.0.0.1 AFC-2100  Added numbers for email tracking events in Mass mailing kanban and form view.\n
@@ -9,6 +9,7 @@
 	 v12.1.0.1 AFC-3042  Fixed the total click calulation
 	 v12.1.0.2 AFC-2943 Added clicks ratio to the report. Added to that we calulate the total number of clicks, so every
 	 time a link has been pressed in a mail, and added that to the report.
+	 v12.1.0.3 AFC-3125 Moved link tracker code to mass_mailing_statistics_af module.
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
@@ -16,12 +17,14 @@
     "contributors": ["Vertel AB"],
     "depends": [
         "mass_mailing",
-        'mass_mailing_sender_logs',#until the link tracker logic is moved from mass_mailing_sender_logs, then this module needs to depend on it.
+        "link_tracker",
         
     ],
     "data": [
         'views/mass_mailing_views.xml',
         'views/mail_statistics_report_view.xml',
+        'views/link_tracker.xml',
+        'security/ir.model.access.csv',
     ],
     "qweb": [
         'static/src/xml/base.xml',
