071ed8b651731a56b502c2aa5baa51a38ace8aaa
Anders Wallenquist <aw@odooutv12.vertel.se>
2022-01-29 16:02:08 +0000

    mass_mailing_experiments: trigger moved to mass.mail

--- mass_mailing_experiments/__manifest__.py
+++ mass_mailing_experiments/__manifest__.py
@@ -16,16 +16,16 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Email Marketing',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','mass_mailing'],
+    'depends': ['mass_mailing'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
+        # ~ 'views/templates.xml',
     ],
 }
