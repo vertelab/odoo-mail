bdbc45551eb50931a18bec26ed258e9e50f78b5d
Hannah Bergenroth <hannah.bergenroth@arbetsformedlingen.se>
2022-02-03 16:58:09 +0100

    solve merge conflict

--- mass_mailing_unsubscribe_af/static/src/js/unsubscribe.js
+++ mass_mailing_unsubscribe_af/static/src/js/unsubscribe.js
@@ -299,3 +299,65 @@ function clicked(e) {
         e.preventDefault();
     }
 }
+
+
+document.onkeydown = function(evt) {
+    let isEscape;
+    if ("key" in evt) {
+        isEscape = evt.key == "Escape" || evt.key == "Esc";
+    } else {
+        isEscape = evt.keyCode == 27;
+    }
+    if (isEscape) {
+        CloseDialog("unsubscribe");
+    }
+};
+
+function openDialog(e){
+    e.preventDefault()
+    let btnID = "unsubscribe"
+    let body = document.getElementsByTagName("body");
+    let landmarks = document.querySelectorAll("header, main");
+    let overlay = document.getElementById("overlay");
+    let dialog = document.getElementById("dialog");
+    let closeBtn = document.getElementById("dialogClose");
+    let submitBtn = document.getElementById("dialogSubmit");
+    let focusElm = document.getElementById("dialogSubmit");
+
+    for (let i = 0; i < landmarks.length; i++) {
+        landmarks[i].setAttribute("aria-hidden","true");
+    }
+
+    body[0].style.overflow = "hidden";
+    overlay.style.display = "block";
+    overlay.setAttribute("onclick","closeDialog('" + btnID + "');");
+    dialog.setAttribute("aria-modal","true");
+    dialog.removeAttribute("hidden");
+    closeBtn.setAttribute("onclick","closeDialog('" + btnID + "');");
+    submitBtn.setAttribute("onclick", "closeDialog('" + btnID + "', true)");
+    focusElm.focus();
+}
+
+function closeDialog(eID, submit= false) {
+    let body = document.getElementsByTagName("body");
+    let landmarks = document.querySelectorAll("header, main");
+    let overlay = document.getElementById("overlay");
+    let dialog = document.getElementById("dialog");
+    let triggerBtn = document.getElementById(eID);
+    let form = document.getElementById("reason_form");
+
+    for (let i = 0; i < landmarks.length; i++) {
+        landmarks[i].removeAttribute("aria-hidden");
+    }
+
+    body[0].style.overflow = "auto";
+    overlay.style.display = "none";
+    dialog.removeAttribute("aria-modal");
+    dialog.removeAttribute("data-id");
+    dialog.setAttribute("hidden","");
+    triggerBtn.focus();
+
+    if (submit){
+        form.submit();
+    }
+}
