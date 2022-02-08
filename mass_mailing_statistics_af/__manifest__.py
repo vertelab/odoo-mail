4c800c0186afbcd0d07d2e37f472ee79c0435bdf
Haze Lidman <haze.lidman@arbetsformedlingen.se>
2022-02-08 16:15:15 +0100

    mass_mailing_statistics_af: updated version

--- mass_mailing_statistics_af/__manifest__.py
+++ mass_mailing_statistics_af/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Mass mailing statistics AF",
     "summary": "This module will show number for Email tracking events in Mass Mailing.",
-    "version": "12.0.1.0.4",
+    "version": "12.0.1.0.5",
     "category": "Email Marketing",
     "description": """
 	 v12.0.0.1 AFC-2100  Added numbers for email tracking events in Mass mailing kanban and form view.\n
