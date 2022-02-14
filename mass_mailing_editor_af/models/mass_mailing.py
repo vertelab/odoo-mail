0270f1d9d88c004ae9c34f60cb1c203753659209
Heidar <54392632+HeidarSelman@users.noreply.github.com>
2022-02-14 10:14:37 +0100

    Added error popup if user send  a mail without selected template in the editor (#177)

--- mass_mailing_editor_af/models/mass_mailing.py
+++ mass_mailing_editor_af/models/mass_mailing.py
@@ -1,7 +1,7 @@
 import logging
 
 from odoo import models, fields, api, _
-
+from odoo.exceptions import UserError
 class MassMailing(models.Model):
     _inherit = "mail.mass_mailing"
     internal_name = fields.Char(string='Internt namn')
@@ -13,3 +13,11 @@ class MassMailing(models.Model):
         for rec in self:
             res.append((rec.id, '%s' % rec.internal_name))
         return res
+
+
+    @api.multi
+    def put_in_queue(self):
+        for rec in self:
+            if not rec.body_html:
+                raise UserError("Du måste välja en mall")
+        super(MassMailing, self).put_in_queue()
