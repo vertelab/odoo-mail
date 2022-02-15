9a0b926833be85f7a57b71e8e630baae795d6cff
Nils Nyman-Waara <nils.nyman-waara@arbetsformedlingen.se>
2022-02-15 07:34:41 +0100

    12.0-ADKD-AFC-3437-Removing-IsCustomerDefault
    * Moved default email domain to mass_mailing_editor_af
    * Removed some valid model types
    * Removed is customer as default domain for res.partner

--- mass_mailing_count_af/__manifest__.py
+++ mass_mailing_count_af/__manifest__.py
@@ -1,11 +1,12 @@
 {
     "name": "Search Count Extension",
     "summary": "Sets config parameter to correct AF-Groups.",
-    "version": "12.0.1.1.2",
+    "version": "12.0.2.0.0",
     "category": "Email Marketing",
     "description": """
     v 12.0.1.1.1 - AFC-3158 - Deleted repeated group name.\n 
-    v 12.0.1.1.2 - AFC-3259 - Adjust access right for guest group.\n 
+    v 12.0.1.1.2 - AFC-3259 - Adjust access right for guest group.\n
+    v 12.0.2.0.0 - AFC-3437 - Moved add-email filter to editor module. \n
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
--- mass_mailing_editor_af/models/mass_mailing.py
+++ mass_mailing_editor_af/models/mass_mailing.py
@@ -2,10 +2,17 @@ import logging
 
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
+from odoo.tools import safe_eval as eval
+
+_logger = logging.getLogger(__name__)
+
+# Overriding default valid models with those we want.
+MASS_MAILING_BUSINESS_MODELS = ['res.partner', 'mail.mass_mailing.list']
+
 class MassMailing(models.Model):
     _inherit = "mail.mass_mailing"
     internal_name = fields.Char(string='Internt namn')
-
+    mailing_model_id = fields.Many2one('ir.model', string='Recipients Model', domain=[('model', 'in', MASS_MAILING_BUSINESS_MODELS)])
 
     @api.multi
     def name_get(self):
@@ -14,10 +21,39 @@ class MassMailing(models.Model):
             res.append((rec.id, '%s' % rec.internal_name))
         return res
 
-
     @api.multi
     def put_in_queue(self):
         for rec in self:
             if not rec.body_html:
                 raise UserError("Du måste välja en mall")
         super(MassMailing, self).put_in_queue()
+
+    @api.onchange('mailing_domain')
+    def add_email_filter(self):
+        """Added email filter for search domain."""
+        domain_list = eval(self.mailing_domain)
+        email_is_set = ['email', '!=', False]
+        if email_is_set not in domain_list:
+            domain_list.append(email_is_set)
+            self.mailing_domain = repr(domain_list)
+            _logger.debug(f"Updated mailing domain to: {self.mailing_domain}.")
+
+    # Override to remove customer = True from res.partner.
+    @api.onchange('mailing_model_id', 'contact_list_ids')
+    def _onchange_model_and_list(self):
+        mailing_domain = []
+        if self.mailing_model_name:
+            if self.mailing_model_name == 'mail.mass_mailing.list':
+                if self.contact_list_ids:
+                    mailing_domain.append(('list_ids', 'in', self.contact_list_ids.ids))
+                else:
+                    mailing_domain.append((0, '=', 1))
+            elif self.mailing_model_name == 'res.partner':
+                pass
+                #mailing_domain.append(('customer', '=', True))
+            elif 'opt_out' in self.env[self.mailing_model_name]._fields and not self.mailing_domain:
+                mailing_domain.append(('opt_out', '=', False))
+        else:
+            mailing_domain.append((0, '=', 1))
+        self.mailing_domain = repr(mailing_domain)
+        self.body_html = "on_change_model_and_list"
