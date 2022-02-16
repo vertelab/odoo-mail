cbd05847ef7d8aecb06156a5ed4eca4dfbd95471
Heidar <54392632+HeidarSelman@users.noreply.github.com>
2022-02-16 15:57:17 +0100

    Fixed calculation of CTR (#183)

--- mass_mailing_statistics_af/__manifest__.py
+++ mass_mailing_statistics_af/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Mass mailing statistics AF",
     "summary": "This module will show number for Email tracking events in Mass Mailing.",
-    "version": "12.0.1.1.0",
+    "version": "12.0.1.1.1",
     "category": "Email Marketing",
     "description": """
 	 v12.0.0.1 AFC-2100  Added numbers for email tracking events in Mass mailing kanban and form view.\n
@@ -17,6 +17,7 @@
      v12.1.0.8 AFC-3502 Change translation to field
      v12.1.0.9 AFC-3470 Hide replied view and changed filed in the kanban view.
      v12.1.1.0 AFC-3473 Calculate CTOR and add field to views
+     v12.1.1.1 AFC-3506 Fixed calculation of CTR.
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
