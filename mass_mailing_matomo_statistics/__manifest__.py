5681c9bfed819096d3b9585b98e54c14e52097f0
Hannah Bergenroth <hannah.bergenroth@arbetsformedlingen.se>
2022-02-16 15:29:31 +0100

    Add translation

--- mass_mailing_matomo_statistics/__manifest__.py
+++ mass_mailing_matomo_statistics/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Mass mailing Matomo statistics AF",
     "summary": "This module will add snippet in Mass Mailing mail body to add Matomo image for email tracking.",
-    "version": "12.0.0.1.1",
+    "version": "12.0.0.1.2",
     "category": "Email Marketing",
     "description": """
     After installation of this module, Go to Setting >> Technical >> Parameter >> System Parameter. \n
@@ -15,6 +15,7 @@
      v12.0.0.4 AFC-2865 Added bouncing categories and a tree view to show them. There is a button when looking at bounces on a mass_mailing that will show the categories\n
      v12.0.1.0 AFC-3207 hide opt out all button for ADKD guest group\n
      v12.0.1.1 AFC-3074 Add external ID's.\n
+     v12.0.1.2 AFC-3126 Add translation\n
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
