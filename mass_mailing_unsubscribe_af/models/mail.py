13b32a4180325c719e19cf05cbc5449f0c7d3c4d
Haze Lidman <63632561+HazeLidman@users.noreply.github.com>
2022-02-21 14:52:57 +0100

    Update mail.py

--- mass_mailing_unsubscribe_af/models/mail.py
+++ mass_mailing_unsubscribe_af/models/mail.py
@@ -39,5 +39,5 @@ class MassMailing(models.Model):
             record.total_unsubscribers = len(res_blacklist) + len(res_unsubscritption)
 
             if record.sent != 0:
-                record.unsubscription_ratio_percent = 100 * record.total_unsubscribers / record.sent
+                record.unsubscription_ratio_percent = 100 * record.total_unsubscribers / int(record.sent)
                 
