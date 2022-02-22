cf057bfed46f30567183dcc134a6c88a657a6d2c
Nils Nyman-Waara <nils.nyman-waara@arbetsformedlingen.se>
2022-02-22 16:38:26 +0100

    12.0-ADKD-AFC-3471-Updating-CTR-Calculations
    * Changed group_by scheduled_date to sent_date

--- mass_mailing_statistics_af/models/mail_statistics_report.py
+++ mass_mailing_statistics_af/models/mail_statistics_report.py
@@ -23,30 +23,13 @@ class MassMailing(models.Model):
         """
         self = self.sudo()
         tools.drop_view_if_exists(self.env.cr, 'mail_statistics_report')
-        self.env.cr.execute(
-            """
-            SELECT
-                utm_source.name as name,
-                count(ms.sent) as sent,
-                count(ms.bounced) as bounced,
-                count(ms.clicked) as clicked,
-                CAST(count(ms.clicked) AS FLOAT) / nullif(count(ms.sent) - count(ms.bounced), 0) as clicks_ratio
-            FROM
-                    mail_mail_statistics as ms
-                    left join mail_mass_mailing as mm ON (ms.mass_mailing_id=mm.id)
-                    left join mail_mass_mailing_campaign as mc ON (ms.mass_mailing_campaign_id=mc.id)
-                    left join utm_campaign as utm_campaign ON (mc.campaign_id = utm_campaign.id)
-                    left join utm_source as utm_source ON (mm.source_id = utm_source.id)
-                GROUP BY ms.scheduled, utm_source.name, utm_campaign.name, mm.state, mm.email_from
-        """)
-        res = self.env.cr.dictfetchall()
-        for row in res:
-            _logger.warning(f'NILS: {row}')
+        # ToDo: mm.sent_date as scheduled_date should be as sent_date,
+        # ToDo: but needs testing.
         self.env.cr.execute("""
             CREATE OR REPLACE VIEW mail_statistics_report AS (
                 SELECT
                     min(ms.id) as id,
-                    ms.scheduled as scheduled_date,
+                    mm.sent_date as scheduled_date,
                     utm_source.name as name,
                     utm_campaign.name as campaign,
                     count(ms.bounced) as bounced,
@@ -67,5 +50,5 @@ class MassMailing(models.Model):
                     left join mail_mass_mailing_campaign as mc ON (ms.mass_mailing_campaign_id=mc.id)
                     left join utm_campaign as utm_campaign ON (mc.campaign_id = utm_campaign.id)
                     left join utm_source as utm_source ON (mm.source_id = utm_source.id)
-                GROUP BY ms.scheduled, utm_source.name, utm_campaign.name, mm.state, mm.email_from
+                GROUP BY mm.sent_date, utm_source.name, utm_campaign.name, mm.state, mm.email_from
             )""")
