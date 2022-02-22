bcddaea84531be3bce43d4ffa7aa73c2e148bd50
Nils Nyman-Waara <nils.nyman-waara@arbetsformedlingen.se>
2022-02-22 11:16:57 +0100

    12.0-ADKD-AFC-3471-Updating-CTR-Calculations
    * Debug

--- mass_mailing_statistics_af/models/mail_statistics_report.py
+++ mass_mailing_statistics_af/models/mail_statistics_report.py
@@ -1,5 +1,8 @@
+import logging
 
 from odoo import api, fields, models, tools
+
+_logger = logging.getLogger(__name__)
 
 
 class MassMailing(models.Model):
@@ -12,13 +15,34 @@ class MassMailing(models.Model):
 
     @api.model_cr
     def init(self):
+        _logger.warning(f'NILS: {"*"*100}')
         """
         Mass Mail Statistical Report: based on mail.mail.statistics
         that models the various statistics collected for each mailing,
         and mail.mass_mailing model that models the various mailing
         performed.
         """
+        self = self.sudo()
         tools.drop_view_if_exists(self.env.cr, 'mail_statistics_report')
+        self.env.cr.execute(
+            """
+            SELECT
+                utm_source.name as name,
+                count(ms.sent) as sent,
+                count(ms.bounced) as bounced,
+                count(ms.clicked) as clicked,
+                ((1.00000 * count(ms.clicked)) / nullif((1.00000 * (count(ms.sent) - count(ms.bounced))), 0)) as clicks_ratio
+            FROM
+                    mail_mail_statistics as ms
+                    left join mail_mass_mailing as mm ON (ms.mass_mailing_id=mm.id)
+                    left join mail_mass_mailing_campaign as mc ON (ms.mass_mailing_campaign_id=mc.id)
+                    left join utm_campaign as utm_campaign ON (mc.campaign_id = utm_campaign.id)
+                    left join utm_source as utm_source ON (mm.source_id = utm_source.id)
+                GROUP BY ms.scheduled, utm_source.name, utm_campaign.name, mm.state, mm.email_from
+        """)
+        res = self.env.cr.dictfetchall()
+        for row in res:
+            _logger.warning(f'NILS: {row}')
         self.env.cr.execute("""
             CREATE OR REPLACE VIEW mail_statistics_report AS (
                 SELECT
@@ -33,9 +57,9 @@ class MassMailing(models.Model):
                     count(ms.replied) as replied,
                     count(ms.clicked) as clicked,
                     sum(ms.total_clicks) as total_clicks,
-                    ((1.00000 * count(ms.clicked)) / nullif((1.00000 * (count(ms.sent) - count(ms.bounced))),0)) as clicks_ratio,
-                    ((1.00000 * count(ms.clicked)) / nullif((1.00000 * count(ms.opened)),0)) as ctor,
-                    ((1.00000 * count(ms.opened)) / nullif((1.00000 * (count(ms.sent) - count(ms.bounced))),0)) as opened_ratio,
+                    ((1.00000 * count(ms.clicked)) / nullif((1.00000 * (count(ms.sent) - count(ms.bounced))), 0)) as clicks_ratio,
+                    ((1.00000 * count(ms.clicked)) / nullif((1.00000 * count(ms.opened)), 0)) as ctor,
+                    ((1.00000 * count(ms.opened)) / nullif((1.00000 * (count(ms.sent) - count(ms.bounced))), 0)) as opened_ratio,
                     mm.state,
                     mm.email_from
                 FROM
