edbe0aea62c64d0f8b2bbb808b4ac1598fa97431
Sinan Özbek <sinan.ozbek@arbetsformedlingen.se>
2022-02-25 14:05:02 +0100

    Added new snippet design and changed some js and css code for the editor

--- mass_mailing_editor_af/__manifest__.py
+++ mass_mailing_editor_af/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "AF Mass Mailing Snippets",
     "summary": "This version is used to customize the Odoo standard mail body widgets in accordance to 'Af' mail template.",
-    "version": "12.0.0.5.0",
+    "version": "12.0.0.5.1",
     "category": "Email Marketing",
     "description": """
     v12.0.0.0.1 AFC-2067  Added snippets in Mass mailing body.\n
@@ -25,6 +25,7 @@
     v12.0.0.3.0 AFC-3437 Added email to filter, removed unused models in selection, removed is customer as default for res.partner.\n
     v12.0.0.4.0 AFC-3416 Improved the dialog box when sending mass_mailings to show how manny mails that will be sent.\n
     v12.0.0.5.0 AFC-3534 Check for emtpy mail list when sending by mail list.\n
+    v12.0.0.5.1 AFC-3418 New snippet created(id=s_mail_block_header_af_dark) and some css changes. \n
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
