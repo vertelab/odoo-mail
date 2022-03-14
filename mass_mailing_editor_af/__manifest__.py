3d8d37e9ae36b3bd2f804611da0e857474da62de
Hannah Bergenroth <hannah.bergenroth@arbetsformedlingen.se>
2022-03-14 12:24:57 +0100

    change version

--- mass_mailing_editor_af/__manifest__.py
+++ mass_mailing_editor_af/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "AF Mass Mailing Snippets",
     "summary": "This version is used to customize the Odoo standard mail body widgets in accordance to 'Af' mail template.",
-    "version": "12.0.0.5.16",
+    "version": "12.0.0.5.17",
     "category": "Email Marketing",
     "description": """
     v12.0.0.0.1 AFC-2067  Added snippets in Mass mailing body.\n
