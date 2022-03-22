0c2ee0fb61c4d659d66103c6846bcf0d73f0ffda
Hannah Bergenroth <91599102+berhr@users.noreply.github.com>
2022-03-22 11:07:04 +0100

    solve merge conflict (#253)

--- mass_mailing_editor_af/__manifest__.py
+++ mass_mailing_editor_af/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "AF Mass Mailing Snippets",
     "summary": "This version is used to customize the Odoo standard mail body widgets in accordance to 'Af' mail template.",
-    "version": "12.0.0.5.43",
+    "version": "12.0.0.5.44",
     "category": "Email Marketing",
     "description": """
     v12.0.0.0.1 AFC-2067  Added snippets in Mass mailing body.\n
@@ -68,6 +68,7 @@
     v12.0.0.5.41 AFC-3456 Create new snippet(id=s_mail_block_toppyta_small_button)\n
     v12.0.0.5.42 AFC-3454 Create new snippet(id=s_mail_block_toppyta_medium_button)\n
     v12.0.0.5.43 AFC-3587 Fix snippet id=s_mail_block_top_innehall_medium_lanktext\n
+    v12.0.0.5.44 AFC-3583 Fix snippet id=s_mail_block_toppyta_h3_text\n
     """,
     "license": "AGPL-3",
     "maintainer": "Vertel AB",
