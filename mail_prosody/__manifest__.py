488ed585c6ae1dd05c877cf08ece16c07fcfaa7e
Miracle Ayodele <mircleaayodele@gmail.com>
2022-08-16 14:02:27 +0200

    cleanups

--- mail_prosody/__manifest__.py
+++ mail_prosody/__manifest__.py
@@ -1,7 +1,39 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021 Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
-    'name': "odoo-chat",
-    'depends': ['base', 'mail',],
+    'name': 'Odoo Chat',
+    'version': '14.0.0',
+    'category': 'other',
+    'license': 'AGPL-3',
+    'summary': 'Makes it easy to chat with prosody',
+    'description': """Makes it easy to chat with prosody""",
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se',
+    'depends': ['base', 'mail'],
     'data': [
-    ]
+    ],
+    "external_dependencies": {
+        'python': ['odoorpc'],
+    },
+    'auto_install': False,
 }
+# vim:expandtab:smartindent:tabstop=4s:softtabstop=4:shiftwidth=4:
--- mail_prosody/models/mail_channel_search_and_read.py
+++ mail_prosody/models/mail_channel_search_and_read.py
@@ -11,6 +11,7 @@ from odoo.exceptions import ValidationError
 
 _logger = logging.getLogger(__name__)
 
+
 class ChannelSearchRead(models.Model):
     _inherit = "mail.channel"
 
@@ -31,26 +32,30 @@ class ChannelSearchRead(models.Model):
         return res.ids if res else 0
 
     @api.returns('mail.message', lambda value: value.id)
-    def message_post(self, *,
-                     body='', subject=None, message_type='notification',
-                     email_from=None, author_id=None, parent_id=False,
-                     subtype_xmlid=None, subtype_id=False, partner_ids=None, channel_ids=None,
-                     attachments=None, attachment_ids=None,
-                     add_sign=True, record_name=False,
-                     **kwargs):
+    def message_post(self, *, body='', subject=None, message_type='notification', email_from=None, author_id=None,
+                     parent_id=False, subtype_xmlid=None, subtype_id=False, partner_ids=None, channel_ids=None,
+                     attachments=None, attachment_ids=None, add_sign=True, record_name=False, **kwargs):
 
-                    #_logger.error(f"{kwargs=}")
+        res = super().message_post(body=body, subject=subject, message_type=message_type, email_from=email_from,
+                                   author_id=author_id, parent_id=parent_id, subtype_xmlid=subtype_xmlid,
+                                   subtype_id=subtype_id, partner_ids=partner_ids, channel_ids=channel_ids,
+                                   attachments=attachments, attachment_ids=attachment_ids, add_sign=add_sign,
+                                   record_name=record_name, **kwargs)
+        print(kwargs)
 
-                    res = super().message_post(body=body, subject=subject, message_type=message_type,
-                                                email_from=email_from, author_id=author_id, parent_id=parent_id,
-                                                subtype_xmlid=subtype_xmlid, subtype_id=subtype_id, partner_ids=partner_ids,
-                                                channel_ids=channel_ids, attachments=attachments, attachment_ids=attachment_ids,
-                                                add_sign=add_sign, record_name=record_name, **kwargs)
-                    #_logger.error(f"res {res=}")
-                    if res.id and not kwargs.get("prosody"):
-                        url = "https://hoary.vertel.se:5281/rest"
-                        js = {'body': body, 'kind': 'message', 'to': 'dostoevsky@hoary.vertel.se', 
-                              'type': 'chat', 'id': 'ODOOODOO' + str(res.id)}
-                        headers = {'Content-type': 'application/json'}
-                        request_post = requests.post(url, json=js, headers=headers, verify=False, auth=("admin", "admin"))
-                    return res
+        if res.id and not kwargs.get("prosody"):
+            url = "https://lvh.me:5281/rest"
+            recipient_ids = res.channel_ids.mapped('channel_partner_ids') - res.author_id
+            data = {
+                'body': body,
+                'kind': 'message',
+                'to': recipient_ids[0].email,
+                # 'from': res.author_id.email,
+                'type': 'chat',
+                'id': 'ODOOODOO' + str(res.id),
+            }
+            headers = {'Content-type': 'application/json'}
+            # request_post = requests.post(url, json=js, headers=headers, verify=False, auth=("admin", "admin"))
+            request_post = requests.post(url, data=data, verify=False, headers=headers)
+            print(request_post.text)
+        return res
