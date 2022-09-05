3f80aea76fed582a5a9f22909f904281d712c1f6
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-05 15:42:38 +0200

    Update __manifest__.py

--- mail_message_archive/__manifest__.py
+++ mail_message_archive/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo SA, Open Source Management Solution, third party addon
-#    Copyright (C) 2021 Vertel AB (<https://vertel.se>).
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -15,19 +15,25 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 {
-    'name': 'Mail Message Archive',
-    'version': '14.0.0',
-    'category': 'other',
-    'license': 'AGPL-3',
-    'summary': 'Makes it easy to archive information',
-    'description': """Extend mail.message with an active-attribute""",
+    'name': 'Mail: Message Archive',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Makes it easy to archive information.',
+    'category': 'Technical',
+    'description': 'Extend mail.message with an active-attribute.',
+    #'sequence': '1',
     'author': 'Vertel AB',
-    'website': 'https://vertel.se',
+    'website': 'https://vertel.se/apps/odoo-mail/mail_message_archive',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-mail',
     'depends': ['mail'],
     'data': [
         #'mail_templates.xml',
