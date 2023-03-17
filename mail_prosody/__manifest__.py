c4fe28dd420b15c9121c8ef4a9c15f7ba5356c19
iammiracle <miracleaayodele@gmail.com>
2023-03-17 11:11:01 +0100

    manifest fixed

--- mail_prosody/__manifest__.py
+++ mail_prosody/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo SA, Open Source Management Solution, third party addon
-#    Copyright (C) 2021 Vertel AB (<https://vertel.se>).
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -15,27 +15,27 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 {
-    'name': 'Odoo Chat',
-    'version': '14.0.0',
-    'category': 'other',
-    'license': 'AGPL-3',
-    'summary': 'Makes it easy to chat with prosody',
-    'description': """Makes it easy to chat with prosody""",
+    'name': 'Mail: Prosody',
+    'version': '14.0.0.0.0',
+    'summary': 'Odoo chat.',
+    'category': 'Administration',
+    'description': """"
+    Chat in Odoo.
+    """,
     'author': 'Vertel AB',
-    'website': 'https://vertel.se',
+    'website': 'https://vertel.se/apps/odoo-mail/mail_prosody',
+    'images': ['static/description/banner.png'],  # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-mail',
     'depends': ['base', 'mail', 'mail_bot'],
     'data': [
-        "data/ir_config_parameter.xml",
-        "views/mail_channel_view.xml",
-    ],
-    "external_dependencies": {
-        'python': ['odoorpc'],
-    },
-    'auto_install': False,
+    ]
 }
-# vim:expandtab:smartindent:tabstop=4s:softtabstop=4:shiftwidth=4:
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
