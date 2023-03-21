2cd1b190e6144b6e0038c4ae142c79c28b6c0fac
iammiracle <miracleaayodele@gmail.com>
2023-03-21 15:55:31 +0100

    changes to manifest

--- mail_prosody/controllers/main.py
+++ mail_prosody/controllers/main.py
@@ -1,6 +1,8 @@
 import logging
+import json
 
 from odoo import http
+from odoo.http import Response
 
 _logger = logging.getLogger(__name__)
 
@@ -9,3 +11,7 @@ class Prosody(http.Controller):
     @http.route('/api/auth/', methods=['POST'], type='json', auth='none', csrf=False)
     def api_auth_user(self, **kw):
         _logger.warning("Incoming data %s", kw)
+        headers = {'Content-Type': 'application/json'}
+        body = {'results': {'code': 200, 'message': 'OK'}}
+
+        return Response(json.dumps(body), headers=headers)
