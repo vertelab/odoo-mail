8273759cdf06e7b7d8426b98fb83a83c2a8c00df
iammiracle <miracleaayodele@gmail.com>
2023-03-22 10:47:41 +0100

    changes

--- mail_prosody/controllers/main.py
+++ mail_prosody/controllers/main.py
@@ -15,8 +15,8 @@ class Prosody(http.Controller):
         headers = {'Content-Type': 'application/json'}
         body = {'results': {'code': 200, 'message': 'OK'}}
 
-        res = request.session.authenticate('prosody', kw.get('username'), kw.get('password'))
-        print(res)
-        return request.env['ir.http'].session_info()
+        # res = request.session.authenticate('prosody', kw.get('username'), kw.get('password'))
+        # print(res)
+        # return request.env['ir.http'].session_info()
 
-        # return Response(json.dumps(body), headers=headers)
+        return Response(json.dumps(body), headers=headers)
