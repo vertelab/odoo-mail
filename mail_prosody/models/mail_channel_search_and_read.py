be3428bfcc3b30e12344712443c13ea8bf50447b
iammiracle <miracleaayodele@gmail.com>
2023-03-30 15:08:45 +0200

    clean logs

--- mail_prosody/models/mail_channel_search_and_read.py
+++ mail_prosody/models/mail_channel_search_and_read.py
@@ -48,7 +48,7 @@ class ChannelSearchRead(models.Model):
         url = f"{url}/message/groupchat/{self.channel_email}"
         headers = {'Content-type': 'text/plain'}
         requests.post(url, headers=headers, data=body, auth=(self.env.user.login, admin_passwd), verify=False)
-        response = requests.request("POST", url, headers=headers, data=body)
+        response = requests.request("POST", url, headers=headers, data=body, auth=(self.env.user.login, admin_passwd), verify=False)
         _logger.warning("response+++++ %s", response.text)
 
     def public_p2p_message(self, url, data, admin_passwd):
