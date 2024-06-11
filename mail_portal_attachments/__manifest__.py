e677da45cf5c30087533fd0c143f41be8a52ba6a
iammiracle <miracleaayodele@gmail.com>
2024-06-11 14:52:40 +0200

    allow attachments from portal user to be received in mailbox

---
+++ mail_portal_attachments/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Mail: Portal Attachments',
+    'version': '14.0.0.0.0',
+    'summary': 'Mail Portal Attachments',
+    'category': 'Administration',
+    'description': """"
+        Mail Portal Attachments.
+    """,
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-mail/mail_portal_attachments',
+    'images': ['static/description/banner.png'],  # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-mail',
+    'depends': ['base', 'mail', 'portal'],
+    'data': [
+    ]
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
