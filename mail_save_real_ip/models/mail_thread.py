a45acfe3b1e31d31bfc86d1f8c2e0e4f42d80f9b
Marcus Jakobsson <jakmar@odooutv14.vertel.se>
2024-08-13 16:30:55 +0200

    new module for tracking who created an record

---
+++ mail_save_real_ip/models/mail_thread.py
@@ -0,0 +1,33 @@
+from odoo import models, fields, api, tools, _
+import logging
+import os
+from odoo.http import request
+
+_logger = logging.getLogger(__name__)
+
+class MailThread(models.AbstractModel):
+    
+    _inherit = "mail.thread"
+
+    def _get_creation_message(self)
+        res = super(MailThread,self)._get_creation_message()
+        #return _('%s created') % doc_name
+
+        _logger.warning(f"{request.httprequest.headers=}")
+        _logger.warning(f"{os.environ=}")
+        _logger.warning(f"{[r for r in request.httprequest.headers if 'X-Real-Ip' in r]}")
+        _logger.warning(f"{[r for r in request.httprequest.headers if 'User-Agent' in r]}")
+        # ~ _logger.warning(f"{[r for r in request.httprequest.headers.items()]}")
+        return _('%s \n%s') % (res,[r for r in request.httprequest.headers if 'X-Real-Ip' in r])
+
+
+    def _creation_message(self):
+        res = super(MailThread,self)._creation_message()
+        #return _('%s created') % doc_name
+
+        _logger.warning(f"{request.httprequest.headers=}")
+        _logger.warning(f"{os.environ=}")
+        _logger.warning(f"{[r for r in request.httprequest.headers if 'X-Real-Ip' in r]}")
+        _logger.warning(f"{[r for r in request.httprequest.headers if 'User-Agent' in r]}")
+        # ~ _logger.warning(f"{[r for r in request.httprequest.headers.items()]}")
+        return _('%s \n%s') % (res,[r for r in request.httprequest.headers if 'X-Real-Ip' in r])
